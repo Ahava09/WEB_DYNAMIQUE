c6c5ac59ec5095fa51ca41bdb3d7a87d51ed6aac
ahava <minoraherinirina72@gmail.com>
2023-03-14 08:10:12 +0100

    1

---
+++ Web/src/java/etu1985/model/Student.java
@@ -0,0 +1,21 @@
+package etu1985.model;
+
+public class Student {
+    String firstame;
+    String name;
+    
+    public String getFirstame() {
+        return firstame;
+    }
+    public void setFirstame(String firstame) {
+        this.firstame = firstame;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
