b27023fef52cfc5acf40130f078930d6af3552fe
ahava <minoraherinirina72@gmail.com>
2023-03-14 07:28:41 +0100

    fini

---
+++ Web/src/java/etu1985/framework/Mapping.java
@@ -0,0 +1,19 @@
+package etu1985.framework;
+
+public class Mapping{
+    String classname;
+    String method;
+
+    public String getMethod() {
+        return method;
+    }
+    public String getClassname() {
+        return classname;
+    }
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
