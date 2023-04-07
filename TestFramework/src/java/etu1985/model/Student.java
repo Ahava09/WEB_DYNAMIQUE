e1d66fdca5d131ff8cc0b460d70c8b704439c65b
ahava <minoraherinirina72@gmail.com>
2023-04-07 06:47:53 +0200

    .

---
+++ TestFramework/src/java/etu1985/model/Student.java
@@ -0,0 +1,33 @@
+package etu1985.model;
+import etu1985.framework.Url;
+import etu1985.framework.servlet.ModelView;
+
+public class Student {
+    String firstame;
+    String name;
+    
+    public String getFirstame() {
+        return firstame;
+    }
+    public void setFirstame(String firstame) {
+        this.firstame = firstame;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(String firstame,String name){
+        this.setFirstame(firstame);
+        this.setName(name);
+    }
+
+    @Url(url="findAllStudent")
+    public ModelView findAll(){
+       ModelView mv = new ModelView("page/all");
+        return mv;
+    }
+    
+}
